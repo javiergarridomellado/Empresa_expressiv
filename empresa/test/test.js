716439e8f488f17aa6d72f4841c6f34f3de53bb4
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2015-10-24 12:20:09 +0200

    testeo mocha

---
+++ empresa/test/test.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+web = require(__dirname+"/../app.js");
+
+describe('WebPracticas', function(){
+    describe('WebPracticas', function(){
+        it('Debe cargar app.js', function(){
+            assert(web, "Cargado, aca dejo mis dies");
+        });
+    });
+});
